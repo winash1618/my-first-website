fb79a0a01315e0bb3618ec76702d17a2392a2dec
winash786 <46051448+winash786@users.noreply.github.com>
2020-10-19 17:41:53 +0530

    klk

---
+++ validate.js
@@ -0,0 +1,30 @@
+function validateForm() {
+    if (document.forms["check"]["fname"].value== "") {
+        alert("Enter your name");
+        return false;
+    }
+    else if (document.forms["check"]["id"].value== "") {
+        alert("Enter your email id");
+        return false;
+    }
+    else if (document.forms["check"]["phone"].value== "") {
+        alert("Enter your phone number");
+        return false;
+    }
+    else if (document.forms["check"]["qual"].value== "") {
+        alert("Enter your qualification");
+        return false;
+    }
+    else if (document.forms["check"]["prog"].value== "") {
+        alert("Enter the program you are applying for");
+        return false;
+    }
+    else {
+        alert("Submission successful, try another")
+        return true;
+    }
+}
+$(function() {
+    $(#header).load("header.html");
+    $(#sidebar).load("sidebar.html");
+})
